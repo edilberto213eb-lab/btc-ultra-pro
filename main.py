5ca7c2f837214e31dbdf99dc29c9b33ee6d8de58
edilberto213eb-lab <edilberto213eb@gmail.com>
2026-09-16 07:59:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1065,8 +1065,10 @@ def bot_loop():
 # =========================================================
 
 
-    bot_thread = threading.Thread(target=bot_loop, daemon=True)
+bot_thread = threading.Thread(
+    target=bot_loop,
+    daemon=True)
+
 bot_thread.start()
-print(">>> BOT THREAD INICIADO <<<")
-print(f">>> TOKEN existe? {bool(TOKEN)}")
-print(f">>> CHAT_ID existe? {bool(CHAT_ID)}")
+
+    
