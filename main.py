717ca114e06f6b04a176efb724825de4196eab18
edilberto213eb-lab <edilberto213eb@gmail.com>
2026-09-17 19:58:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def health():
 def send_telegram(message):
    r = requests.post(url, data=data)
 print("Telegram:", r.status_code, r.text)
-    if not TOKEN or not CHAT_ ID:
+    if not TOKEN or not CHAT_ID:
     print("Faltan las variables de Telegram")
         return
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
